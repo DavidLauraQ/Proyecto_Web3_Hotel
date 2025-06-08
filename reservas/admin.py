c1c2f2672a67f115e50bba1b09c21354a17581db
roy4L <royalejan26@gmail.com>
2025-06-08 17:44:55 -0400

    Avance: se agregó el módulo de reservas

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente,Reserva,Habitacion,TipoHabitacion
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Reserva)
+admin.site.register(Habitacion)
+admin.site.register(TipoHabitacion)
