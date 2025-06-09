6e04e1ecaced88ebd08544d94f54e60e60032dcc
DavidLauraQ <davidlaura5000@gmail.com>
2025-06-09 17:42:22 -0400

    modiificacion de archivos

--- reservas/urls.py
+++ reservas/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.home, name='home'),
     path('clientes/', views.lista_clientes, name='clientes'),
     path('habitaciones/', views.lista_habitaciones, name='habitaciones'),
     path('reservas/', views.lista_reservas, name='reservas'),
